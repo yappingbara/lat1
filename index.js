34007195993dec21d3e671414249f52117b173df
abhi <abhinayaleclerc@gmail.com>
2026-09-09 11:17:46 +0700

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const cart = [ { title: "Laptop", price: 1000, discountPercent: 10 }, 
-    { title: "Mouse", price: 20, discountPercent: 5 }, 
-    { title: "Keyboard", price: 50, discountPercent: 0 } ];
+const cart = [
+    { title: "Laptop", price: 1000, discountPercent: 10 },
+    { title: "Mouse", price: 20, discountPercent: 5 },
+    { title: "Keyboard", price: 50, discountPercent: 0 }
+];
 
-function calculateDiscountedPrice(price, discountPercent) { 
-    return price - (price * discountPercent) / 100
-   
+function calculateDiscountedPrice(price, discountPercent) {
+    return price - (price * discountPercent) / 100;
 }
+
 const result = [];
+
 function applyDiscounts(cart) {
-      
-     for (const item of cart){
-       result.push(
-       calculateDiscountedPrice(item.price,item.discountPercent)
-       )
-
-       }
-
-     
-     return result
+    for (const item of cart) {
+        result.push(
+            calculateDiscountedPrice(item.price, item.discountPercent) + " harga akhir" + ` ${item.title}`
+        );
     }
-    console.log(applyDiscounts)
-    
+
+    return result;
+}
+
+console.log(applyDiscounts(cart));
